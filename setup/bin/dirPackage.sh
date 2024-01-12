7e0807b2816952c956c69dcf1fc2136775446453
rajpatel5521 <141277888+rajpatel5521@users.noreply.github.com>
2024-01-12 12:59:08 -0600

    Update dirPackage.sh

--- setup/bin/dirPackage.sh
+++ setup/bin/dirPackage.sh
@@ -3,4 +3,20 @@ export CDPATH=.:~
 
 declare -a dirStack
 
-len=${#dirStack[@]}
+function pd() {
+  local len=${#dirStack[@]}
+  local p=$(pwd)
+  while [ $len -gt 0 ]; do
+    local s=$dirStack[$len]
+    if [  "p" = "$s"  ]; then
+      break
+    fi
+    let len=len-1
+  done
+  if [ $len -eq 0 ]; then
+    local len=${#dirStack[@]}
+    let len=len+1
+    dirStack[$len]=$(pwd)
+    chdir $1
+  fi
+}
